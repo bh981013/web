8f188bfc67b9af54efa223364864276d75a601e7
bhryan <bh981013@naver.com>
2022-03-03 16:46:27 +0900

    add func practice2

--- 22.03.03/js/test.js
+++ 22.03.03/js/test.js
@@ -1,8 +1,21 @@
+
 //is Empty 함수 구현
-function isEmpty(object){
-    return Boolean(Object.keys(object).length);
+function practice1(){
+    function isEmpty(object){
+        return Boolean(Object.keys(object).length);
+    }
+    let schedule = {};
+    console.log(isEmpty(schedule));
+    schedule["8:30"] = "get up";
+console.log(isEmpty(schedule));
 }
-let schedule = {};
-console.log(isEmpty(schedule));
-schedule["8:30"] = "get up";
-console.log(isEmpty(schedule));
+
+function practice2(){
+    user = {
+        name : "myName",
+        "getName": "otherName"
+    }
+    console.log(user.name);
+    console.log(user["getName"]);
+}
+practice2();
